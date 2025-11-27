32977887b0df1de75d84036fec4e3308cf183b86
ArizaTrizniak <arizatrizniak@gmail.com>
2025-11-26 21:13:07 -0600

    Refactor TextIcon to accept customizable size, color, and strokeWidth props, and update SVG paths for improved rendering

--- src/icons/Text.tsx
+++ src/icons/Text.tsx
@@ -1,20 +1,23 @@
 import * as React from "react";
-import type { SVGProps } from "react";
-export const TextIcon = (props: SVGProps<SVGSVGElement>) => (
+import type { IconProps } from "../types";
+export const TextIcon:React.FC<IconProps> = ({
+     size = 24,
+     color = "black",
+     strokeWidth = 2,
+     ...props
+}) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    fill="none"
     viewBox="0 0 24 24"
-    width="1em"
-    height="1em"
+    fill={color}
+    width={size}
+    height={size}
     {...props}
   >
     <path
       stroke="#000"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={3}
-      d="M9.5 1.5v20m-3.429 0h6.858M17.5 4.833V1.5h-16v3.333"
+      strokeMiterlimit={10}
+      d="M6.25 11.128h3.833M15.833 7.295h4.792M15.833 11.128h4.792M3.375 14.961h17.25M3.375 18.708h17.25M4.881 3.213l6.572.087M8.167 11.042V3.375M5.292 3.375v.958M15.833 3.461l4.6.07M.5 3.375C.5 1.785 2.642.5 5.292.5M23.5 3.375C23.5 1.785 21.358.5 18.708.5M.5 18.708A4.787 4.787 0 0 0 5.292 23.5M23.5 18.708a4.787 4.787 0 0 1-4.792 4.792M11.042 3.375v.958"
     />
   </svg>
 );
